0dfe2ed34d8af1aef8c386c7a33deb58ade46417
Vishal Kumar Mitra <Vishal Kumar Mitra>
2017-10-04 02:25:10 +0530

    second commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var typeNumber = 4;
 var errorCorrectionLevel = 'L';
 var qr = qrcode(typeNumber, errorCorrectionLevel);
-qr.addData('http://192.168.0.108/resume/resume.pdf');
+qr.addData('https://github.com/VishalKumarMitra786/resume/resume/resume.pdf');
 qr.make();
 document.getElementById('qrCode').innerHTML = qr.createImgTag();
