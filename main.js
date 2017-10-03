74d9b4aab5cf3b456054e612f1e6312c416a4a32
Vishal Kumar Mitra <Vishal Kumar Mitra>
2017-10-04 02:31:25 +0530

    renamed host url

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var typeNumber = 4;
 var errorCorrectionLevel = 'L';
 var qr = qrcode(typeNumber, errorCorrectionLevel);
-qr.addData('https://github.com/VishalKumarMitra786/resume/resume/resume.pdf');
+qr.addData('https://vishalkumarmitra786.github.io/resume/resume/resume.pdf');
 qr.make();
 document.getElementById('qrCode').innerHTML = qr.createImgTag();
